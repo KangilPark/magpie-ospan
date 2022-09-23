47ba71473e747180cb411a6b751057877326857a
KangilPark <kangil.park@huskers.unl.edu>
2022-09-23 11:11:16 -0500

    Update 06_main.js

--- 06_main.js
+++ 06_main.js
@@ -52,7 +52,7 @@ $("document").ready(function() {
         // Here, you can specify all information for the deployment
         deploy: {
             experimentID: "2",
-            serverAppURL: "https://sereslstudies.herokuapp.com/api/submit_experiment/",
+            serverAppURL: "https://seresl-studies.herokuapp.com/api/submit_experiment/",
             // Possible deployment methods are:
             // "debug" and "directLink"
             // As well as "MTurk", "MTurkSandbox" and "Prolific"
